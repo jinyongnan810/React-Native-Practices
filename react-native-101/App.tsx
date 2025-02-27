42c9c0e59c5d63a8155bb5999fe4c68a6d8939ad
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-27 17:08:09 +0900

    basic functions with ScrollView and KeyboardAvoidingView

--- react-native-101/App.tsx
+++ react-native-101/App.tsx
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [goalInputText, setGoalInputText] = useState("");
+  const [goals, setGoals] = useState<string[]>([]);
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView>
+        <View style={styles.enterGoalArea}>
+          <TextInput
+            style={styles.goalInput}
+            placeholder="Enter your goal!"
+            value={goalInputText}
+            autoCorrect={false}
+            autoComplete="off"
+            onChangeText={(e) => {
+              setGoalInputText(e);
+            }}
+          />
+          <Button
+            title="Add"
+            onPress={() => {
+              setGoals((current) => [...current, goalInputText]);
+              setGoalInputText("");
+            }}
+          />
+        </View>
+        <View style={styles.goalList}>
+          {goals.map((goal, index) => (
+            <Text style={styles.goalItem} key={index}>
+              {goal}
+            </Text>
+          ))}
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100,
+    paddingHorizontal: 16,
+  },
+  enterGoalArea: {
+    // flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  goalInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 12,
+    width: "80%",
+    fontSize: 20,
+    marginRight: 12,
+  },
+  goalList: {
+    // flex: 7,
+  },
+  goalItem: {
+    fontSize: 16,
+    padding: 12,
+    marginVertical: 6,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 4,
   },
 });
