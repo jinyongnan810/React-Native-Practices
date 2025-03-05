86777c4c24d03920d5fed1986f487e9ed4ef980d
Kin Yuunan <yuunan.kin@gmail.com>
2025-03-05 16:32:17 +0900

    home screen styling

--- mini-game/App.tsx
+++ mini-game/App.tsx
@@ -1,5 +1,29 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet } from "react-native";
 import HomeScreen from "./pages/HomeScreen";
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <LinearGradient style={styles.screen} colors={["#f7287b", "#ff7e5f"]}>
+        <ImageBackground
+          source={require("./assets/background.png")}
+          resizeMode="cover"
+          style={styles.screen}
+          imageStyle={{ opacity: 0.2 }}
+        >
+          <HomeScreen />
+        </ImageBackground>
+      </LinearGradient>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    // backgroundColor: "#f7287b",
+  },
+});
--- mini-game/pages/HomeScreen.tsx
+++ mini-game/pages/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const HomeScreen = () => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -15,19 +16,27 @@ const HomeScreen = () => {
           autoComplete="off"
           autoCorrect={false}
           keyboardType="number-pad"
+          maxLength={2}
         />
         <View style={styles.enterAreaButtons}>
-          <Button title="Reset" onPress={() => setEnteredValue("")} />
-          <Button
-            title="Confirm"
-            onPress={() => {
-              const num = parseInt(enteredValue);
-              if (isNaN(num) || num < 1 || num > 99) {
-                alert("Please enter a number between 1 and 99.");
-                return;
-              }
-            }}
-          />
+          <View style={{ flex: 1 }}>
+            <PrimaryButton onClick={() => setEnteredValue("")}>
+              Reset
+            </PrimaryButton>
+          </View>
+          <View style={{ flex: 1 }}>
+            <PrimaryButton
+              onClick={() => {
+                const num = parseInt(enteredValue);
+                if (isNaN(num) || num < 1 || num > 99) {
+                  alert("Please enter a number between 1 and 99.");
+                  return;
+                }
+              }}
+            >
+              Confirm
+            </PrimaryButton>
+          </View>
         </View>
       </View>
     </View>
@@ -37,10 +46,7 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
-    backgroundColor: "#f7287b",
     padding: 16,
   },
   title: {
@@ -49,15 +55,24 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: "white",
     padding: 10,
+    marginTop: 200,
   },
   enterArea: {
-    marginTop: 20,
-    padding: 24,
+    flex: 1,
+    marginTop: 24,
+    marginHorizontal: 16,
+    paddingVertical: 24,
+    paddingHorizontal: 12,
     backgroundColor: "#11287b",
     borderRadius: 10,
-    width: "100%",
     alignItems: "center",
-    height: 200,
+    justifyContent: "center",
+    maxHeight: 240,
+    elevation: 10, // Android only
+    shadowRadius: 10, // iOS only
+    shadowColor: "black", // iOS only
+    shadowOpacity: 0.7, // iOS only
+    shadowOffset: { width: 0, height: 2 }, // iOS only
   },
   enterAreaTitle: {
     fontSize: 20,
