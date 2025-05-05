4ca9aba495b173b3ebb568ab2783567d2ff1cd03
Kin Yuunan <yuunan.kin@gmail.com>
2025-05-05 15:19:09 +0900

    avoid keyboard

--- expense-tracker/components/auth/AuthContents.tsx
+++ expense-tracker/components/auth/AuthContents.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Alert, Button, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 import Colors from "../../constants";
 import MyButton from "../MyButton";
 import MyInput from "../MyInput";
@@ -17,8 +24,9 @@ const AuthContents = ({ isLogin, onSwitch, onAuthenticate }: Props) => {
     password: false,
   });
   function submitHandler() {
+    setCredentialsInvalid({ email: false, password: false });
     const email = enteredEmail.trim();
-    const password = enteredEmail.trim();
+    const password = enteredPassword.trim();
 
     const emailIsValid = email.includes("@");
     const passwordIsValid = password.length > 6;
@@ -34,54 +42,59 @@ const AuthContents = ({ isLogin, onSwitch, onAuthenticate }: Props) => {
     onAuthenticate(email, password);
   }
   return (
-    <View style={styles.container}>
-      <View style={styles.authContent}>
-        <View style={styles.form}>
-          <View>
-            <MyInput
-              label="Email Address"
-              onSetValue={(v) => setEnteredEmail(v)}
-              value={enteredEmail}
-              type="email-address"
-              isInvalid={credentialsInvalid.email}
-            />
-            <MyInput
-              label="Password"
-              onSetValue={(v) => setEnteredPassword(v)}
-              secure={true}
-              value={enteredPassword}
-              type="default"
-              isInvalid={credentialsInvalid.password}
-            />
-            <View style={styles.buttons}>
-              <MyButton onClick={submitHandler} type="primary">
-                {isLogin ? "Log In" : "Sign Up"}
-              </MyButton>
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView behavior="position" style={styles.container}>
+        <View style={styles.authContent}>
+          <View style={styles.form}>
+            <View>
+              <MyInput
+                label="Email Address"
+                onSetValue={(v) => setEnteredEmail(v)}
+                value={enteredEmail}
+                type="email-address"
+                isInvalid={credentialsInvalid.email}
+              />
+              <MyInput
+                label="Password"
+                onSetValue={(v) => setEnteredPassword(v)}
+                secure={true}
+                value={enteredPassword}
+                type="default"
+                isInvalid={credentialsInvalid.password}
+              />
+              <View style={styles.buttons}>
+                <MyButton onClick={submitHandler} type="primary">
+                  {isLogin ? "Log In" : "Sign Up"}
+                </MyButton>
+              </View>
             </View>
           </View>
+          <View style={styles.buttons}>
+            <Button
+              title={isLogin ? "Create a new user" : "Log in instead"}
+              onPress={onSwitch}
+              color={"#fff"}
+            />
+          </View>
         </View>
-        <View style={styles.buttons}>
-          <Button
-            title={isLogin ? "Create a new user" : "Log in instead"}
-            onPress={onSwitch}
-            color={"#fff"}
-          />
-        </View>
-      </View>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     padding: 16,
   },
   authContent: {
-    width: "100%",
-    marginTop: 64,
+    flex: 1,
+    minWidth: "100%",
+    marginTop: 300,
     marginHorizontal: 32,
     padding: 16,
     borderRadius: 8,
